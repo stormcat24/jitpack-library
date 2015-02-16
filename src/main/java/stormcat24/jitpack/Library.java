0c576053b436b5fb116fdf7a8fe8ff2b8642eb66
stormcat24 <a.yamada24@gmail.com>
2015-02-16 22:49:12 +0900

    1.0

---
+++ src/main/java/stormcat24/jitpack/Library.java
@@ -0,0 +1,16 @@
+package stormcat24.jitpack;
+
+/**
+ * @author stormcat24
+ */
+public final class Library {
+
+    public static void foo() {
+        System.out.println("foo");
+    }
+
+    public static void bar() {
+        System.out.println("foo");
+    }
+
+}
